7221a9cf7667f83c057c9b23b14440b65d0a4185
tejomaygadgil <tejomaygadgil@gmail.com>
2023-12-12 09:52:35 -0800

    Add hidden files to telescope find_files

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,5 +1,9 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>f', builtin.find_files, {})
+vim.keymap.set('n', '<leader>f', function()
+   builtin.find_files({
+        hidden = true,
+    })
+end, {})
 vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>b', builtin.buffers, {})
 vim.keymap.set('n', '<leader>t', vim.cmd.InspectTree, {})
--- nvim/lua/tej/init.lua
+++ nvim/lua/tej/init.lua
@@ -5,7 +5,9 @@ require("tej.lazy")
 vim.api.nvim_create_autocmd("VimEnter", {
   callback = function()
     if vim.fn.argv(0) == "" then
-      require("telescope.builtin").find_files()
+      require("telescope.builtin").find_files({
+        hidden = true,
+        })
     end
   end,
 })
