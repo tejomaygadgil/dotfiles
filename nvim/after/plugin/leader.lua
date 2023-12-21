545a8db474114da2a6612cff656614ca67e3f996
Tejomay Gadgil <tejomaygadgil@gmail.com>
2023-12-21 11:20:53 -0800

    Formatting

--- nvim/after/plugin/leader.lua
+++ nvim/after/plugin/leader.lua
@@ -1,12 +1,15 @@
 -- Set up leader key
-vim.g.mapleader = " "
+vim.g.mapleader = ' '
+
+-- File explorer
+vim.keymap.set('n', '<leader>e', vim.cmd.Ex)
 
 -- Telescope
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', function()
-   builtin.find_files({
-        hidden = true,
-    })
+  builtin.find_files({
+    hidden = true,
+  })
 end, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
