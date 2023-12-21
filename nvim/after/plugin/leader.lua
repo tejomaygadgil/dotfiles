adce1f771b2d290ce59a399cba67ca4d4a9bed76
Tejomay Gadgil <tejomaygadgil@gmail.com>
2023-12-21 10:21:24 -0800

    Reconfigure leader keymap

--- nvim/after/plugin/leader.lua
+++ nvim/after/plugin/leader.lua
@@ -1,21 +1,21 @@
 -- Set up leader key
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
-
--- InspectTree
-vim.keymap.set('n', '<leader>t', vim.cmd.InspectTree, {})
 
 -- Telescope
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>f', function()
+vim.keymap.set('n', '<leader>ff', function()
    builtin.find_files({
         hidden = true,
     })
 end, {})
-vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>b', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+
+-- Open file explorer
+vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+
+-- InspectTree
 vim.keymap.set('n', '<leader>t', vim.cmd.InspectTree, {})
-vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle, {})
 
 -- Undotree
 vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle, {})
