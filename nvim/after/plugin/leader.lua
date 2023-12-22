d556b16baf28c24f72e7dd22381804a95e185c8d
Tejomay Gadgil <tejomaygadgil@gmail.com>
2023-12-21 18:16:28 -0800

    Clean up leader combos

--- nvim/after/plugin/leader.lua
+++ nvim/after/plugin/leader.lua
@@ -5,21 +5,22 @@ vim.g.mapleader = ' '
 vim.keymap.set('n', '<leader>e', vim.cmd.Ex)
 
 -- Telescope
-local builtin = require('telescope.builtin')
+local ts = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', function()
-  builtin.find_files({
+  ts.find_files({
     hidden = true,
   })
 end, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>s', ts.live_grep, {})
+vim.keymap.set('n', '<leader>b', ts.buffers, {})
+vim.keymap.set('n', '<leader>tt', ':Telescope<CR>', {})
 
 -- Git signs
 local gs = require('gitsigns')
 vim.keymap.set({ 'n', 'v' }, '<leader>gs', ':Gitsigns stage_hunk<CR>')
 vim.keymap.set({ 'n', 'v' }, '<leader>gr', ':Gitsigns reset_hunk<CR>')
-vim.keymap.set('n', '<leader>gS', gs.stage_buffer)
 vim.keymap.set('n', '<leader>gu', gs.undo_stage_hunk)
+vim.keymap.set('n', '<leader>gS', gs.stage_buffer)
 vim.keymap.set('n', '<leader>gR', gs.reset_buffer)
 vim.keymap.set('n', '<leader>gp', gs.preview_hunk)
 vim.keymap.set('n', '<leader>gb', function() gs.blame_line({ full = true }) end)
@@ -27,7 +28,7 @@ vim.keymap.set('n', '<leader>gd', gs.diffthis)
 vim.keymap.set('n', '<leader>gD', function() gs.diffthis('~') end)
 
 -- InspectTree
-vim.keymap.set('n', '<leader>t', vim.cmd.InspectTree, {})
+-- vim.keymap.set('n', '<leader>t', vim.cmd.InspectTree, {})
 
 -- Undotree
 vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle, {})
