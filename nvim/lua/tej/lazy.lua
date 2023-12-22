22206f635383815f4055451377452f0f8d0e545b
Tejomay Gadgil <tejomaygadgil@gmail.com>
2023-12-21 20:52:06 -0800

    Fix ctrl-n and ctrl-p bug in insert mode
    https://vi.stackexchange.com/a/42733

--- nvim/lua/tej/lazy.lua
+++ nvim/lua/tej/lazy.lua
@@ -160,7 +160,10 @@ local lsp_zero = require('lsp-zero')
 lsp_zero.on_attach(function(client, bufnr)
   -- see :help lsp-zero-keybindings
   -- to learn the available actions
-  lsp_zero.default_keymaps({ buffer = bufnr })
+  lsp_zero.default_keymaps({
+    buffer = bufnr,
+    omit = { '^n', '^p' },
+  })
 end)
 
 require('mason').setup({})
