3c744c198401ba377ac5396c353325419f0b6bdb
Tejomay Gadgil <tejomaygadgil@gmail.com>
2023-12-21 19:36:56 -0800

    Clean up leader

--- nvim/after/plugin/leader.lua
+++ nvim/after/plugin/leader.lua
@@ -6,21 +6,17 @@ vim.keymap.set('n', '<leader>e', vim.cmd.Ex)
 
 -- Telescope
 local ts = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', function()
-  ts.find_files({
-    hidden = true,
-  })
-end, {})
-vim.keymap.set('n', '<leader>fe', ':Telescope oldfiles<CR>', {})
-vim.keymap.set('n', '<leader>s', ts.live_grep, {})
-vim.keymap.set('n', '<leader>b', ts.buffers, {})
-vim.keymap.set('n', '<leader>tt', ':Telescope<CR>', {})
+vim.keymap.set('n', '<leader>ff', function() ts.find_files({ hidden = true, }) end)
+vim.keymap.set('n', '<leader>fe', ts.oldfiles)
+vim.keymap.set('n', '<leader>s', ts.live_grep)
+vim.keymap.set('n', '<leader>b', ts.buffers)
+vim.keymap.set('n', '<leader>tt', ':Telescope<CR>')
 
 -- Easy picker
-vim.keymap.set('n', '<leader>ge', ':Easypick unstaged_changes<CR>', {})
+vim.keymap.set('n', '<leader>ge', ':Easypick unstaged_changes<CR>')
 
 -- Lazygit
-vim.keymap.set('n', '<leader>gg', ':LazyGit<CR>', {})
+vim.keymap.set('n', '<leader>gg', ':LazyGit<CR>')
 
 -- Git signs
 local gs = require('gitsigns')
@@ -30,12 +26,12 @@ vim.keymap.set('n', '<leader>gu', gs.undo_stage_hunk)
 vim.keymap.set('n', '<leader>gS', gs.stage_buffer)
 vim.keymap.set('n', '<leader>gR', gs.reset_buffer)
 vim.keymap.set('n', '<leader>gp', gs.preview_hunk)
-vim.keymap.set('n', '<leader>gb', function() gs.blame_line({ full = true }) end)
+vim.keymap.set('n', '<leader>gb', function() gs.blame_line({ full = true, }) end)
 vim.keymap.set('n', '<leader>gd', gs.diffthis)
 vim.keymap.set('n', '<leader>gD', function() gs.diffthis('~') end)
 
 -- InspectTree
--- vim.keymap.set('n', '<leader>t', vim.cmd.InspectTree, {})
+-- vim.keymap.set('n', '<leader>t', vim.cmd.InspectTree)
 
 -- Undotree
-vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle, {})
+vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
