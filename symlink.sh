382de91a8c525f305553eac2e5fe2cde539ec165
Tejomay Gadgil <tejomaygadgil@gmail.com>
2024-01-04 10:55:50 -0800

    Add alacritty

--- symlink.sh
+++ symlink.sh
@@ -3,3 +3,4 @@ ln -rsf ./config/.bashrc ~/
 ln -rsf ./config/.tmux.conf ~/
 ln -rsf ./config/.gitconfig ~/
 ln -rsf ./config/.ignore ~/workspace/
+ln -rsf ./config/alacritty.yml ~/.config/alacritty/
