cb0bc31f690ef93d1bd1bcea3a081e24207c8742
Tejomay Gadgil <tejomaygadgil@gmail.com>
2024-01-06 01:27:45 -0800

    Config conda for fish

--- config/config.fish
+++ config/config.fish
@@ -23,3 +23,17 @@ if status is-interactive
     alias dot='cd /home/tejomay/workspace/git/dotfiles'
 
 end
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /home/tejomay/anaconda3/bin/conda
+    eval /home/tejomay/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/home/tejomay/anaconda3/etc/fish/conf.d/conda.fish"
+        . "/home/tejomay/anaconda3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/home/tejomay/anaconda3/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
+
