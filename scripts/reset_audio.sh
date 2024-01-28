d4d26f4269e6aafa09707ae63b72c83f4695a3fd
Tejomay Gadgil <tejomaygadgil@gmail.com>
2024-01-28 11:42:53 -0800

    Add more documentation to the reset bluetooth audio script

--- scripts/reset_audio.sh
+++ scripts/reset_audio.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+# https://support.system76.com/articles/audio/
 systemctl --user restart wireplumber pipewire pipewire-pulse
+rm -r ~/.config/pulse
+# Airplay
 pactl load-module module-raop-discover
+
+# cf. https://askubuntu.com/a/1030961 for BT connection issues
 
