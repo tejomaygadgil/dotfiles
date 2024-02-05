bd1359ec654e37956a0bd12fddd59c86ca49a9d4
Tejomay Gadgil <tejomaygadgil@gmail.com>
2024-02-05 00:12:51 -0800

    Add check to make sure zet dir exists before continuing

--- scripts/zet.sh
+++ scripts/zet.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Inspired by https://github.com/mischavandenburg/dotfiles/blob/main/scripts/zet
 
-zetdir=~/workspace/notes/zet/0-inbox
+zetdir=$ZET
 
 # Get the filename
 prompt_filename() {
@@ -17,25 +17,30 @@ else
 	filename="$(prompt_filename)"
 fi
 
-# Check formatting and uniquness
-pattern="^[a-zA-Z0-9-]+$"
-while ! [[ "$filename" =~ $pattern ]]; do
-  echo "Alnum and dashes only"
-  filename="$(prompt_filename)"
-done
-make_filepath() {
-  echo "$zetdir/$filename.md"
-}
-filepath=$(make_filepath)
-while [ -f "$filepath" ]; do
-  echo "Already exists"
-  filename=$(prompt_filename)
+# Check formatting and uniqueness
+if ! [ -d "$zetdir" ]; then
+  echo "Does not exist: $zetdir"
+  exit 1
+else
+  pattern="^[a-zA-Z0-9-]+$"
+  while ! [[ "$filename" =~ $pattern ]]; do
+    echo "Alnum and dashes only"
+    filename="$(prompt_filename)"
+  done
+  make_filepath() {
+    echo "$zetdir/$filename.md"
+  }
   filepath=$(make_filepath)
-done
+  while [ -f "$filepath" ]; do
+    echo "Already exists"
+    filename=$(prompt_filename)
+    filepath=$(make_filepath)
+  done
 
-# Create and open
-touch $filepath
-{
-  echo -en "# $filename \n\n"
-} >>"$filepath"
-nvim "$filepath"
+  # Create and open
+  touch $filepath
+  {
+    echo -en "# $filename \n\n"
+  } >>"$filepath"
+  nvim "$filepath"
+fi
