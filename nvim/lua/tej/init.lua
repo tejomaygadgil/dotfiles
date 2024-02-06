502ed31b95c19c8370ea23badb49b1511618c28a
Tejomay Gadgil <tejomaygadgil@gmail.com>
2024-02-06 12:40:03 -0800

    Remove telescope at beginning (Use <c-O> instead!)

--- nvim/lua/tej/init.lua
+++ nvim/lua/tej/init.lua
@@ -1,10 +1,10 @@
 require("tej.lazy")
 
 -- Open telescope if no files are opened.
-vim.api.nvim_create_autocmd("VimEnter", {
-  callback = function()
-    if vim.fn.argv(0) == "" then
-      require("telescope.builtin").oldfiles()
-    end
-  end,
-})
+-- vim.api.nvim_create_autocmd("VimEnter", {
+--   callback = function()
+--     if vim.fn.argv(0) == "" then
+--       require("telescope.builtin").oldfiles()
+--     end
+--   end,
+-- })
