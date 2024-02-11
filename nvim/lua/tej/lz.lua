8f0a9f31d86eda73aea535446eb4d15b1b66863b
Tejomay Gadgil <tejomaygadgil@gmail.com>
2024-02-11 00:08:27 -0800

    Refactor get_parent, is_md, is_root for lz

--- nvim/lua/tej/lz.lua
+++ nvim/lua/tej/lz.lua
@@ -1,23 +1,30 @@
 local M = {}
 
+local function get_parent(file_name)
+  local regex = tonumber(file_name:sub(-4, -4)) and "(%d+)$" or "(%a+)$"
+  return file_name:sub(1, -4):gsub(regex, "")
+end
+
+local function is_md(file_name)
+  return file_name:sub(-3) == ".md"
+end
+
+local function is_root(file_name)
+  return file_name:sub(1, -4):match("^%d+$")
+end
+
 function M.move_up()
   local current_file = vim.fn.bufname()
-  if current_file:sub(-3) == ".md" then
-    if not current_file:sub(1, -4):match("^%d+$") then
-      if tonumber(current_file:sub(-4, -4)) then
-        local new_file = current_file:sub(1, -4):gsub("(%d+)$","") .. ".md"
-        vim.cmd('edit ' .. new_file)
-      else
-        local new_file = current_file:sub(1, -4):gsub("(%a+)$","") .. ".md"
-        vim.cmd('edit ' .. new_file)
-      end
+  if is_md(current_file) then
+    if not is_root(current_file) then
+      vim.cmd('edit ' .. get_parent(current_file) .. '.md')
     end
   end
 end
 
 function M.move_down()
   local current_file = vim.fn.bufname()
-  if current_file:sub(-3) == ".md" then
+  if is_md(current_file) then
     local last_char = current_file:sub(-4, -4)
     local new_char = tonumber(last_char) and 'b' or '1'
     local new_file = current_file:sub(1, -4) .. new_char .. '.md'
@@ -27,7 +34,7 @@ end
 
 function M.move_left()
   local current_file = vim.fn.bufname()
-  if current_file:sub(-3) == ".md" then
+  if is_md(current_file) then
     local last_char = current_file:sub(-4, -4)
     if tonumber(last_char) then
       if last_char ~= '0' then
@@ -47,7 +54,7 @@ end
 
 function M.move_right()
   local current_file = vim.fn.bufname()
-  if current_file:sub(-3) == ".md" then
+  if is_md(current_file) then
     local last_char = current_file:sub(-4, -4)
     if tonumber(last_char) then
       local new_char = tostring(tonumber(last_char) + 1)
