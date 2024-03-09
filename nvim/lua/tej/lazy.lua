1a561b030ba06e377350889835aa81739b71d7f9
Tejomay Gadgil <tejomaygadgil@gmail.com>
2024-03-08 23:37:34 -0800

    Add oil git status

--- nvim/lua/tej/lazy.lua
+++ nvim/lua/tej/lazy.lua
@@ -47,9 +47,21 @@ require('lazy').setup(
         view_options = {
           show_hidden = true,
         },
+        win_options = {
+          signcolumn = "yes:2",
+        },
       },
       -- Optional dependencies
       dependencies = { "nvim-tree/nvim-web-devicons" },
+    },
+    {
+      "refractalize/oil-git-status.nvim",
+
+      dependencies = {
+        "stevearc/oil.nvim",
+      },
+
+      config = true,
     },
     {
       "robitx/gp.nvim",
