b3fa379c9cc956e14dffba421a88b91526f1ce42
Tejomay Gadgil <tejomaygadgil@gmail.com>
2024-04-07 12:37:17 -0700

    Consolidate home files and break out bash_aliases

--- symlink.sh
+++ symlink.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-ln -rsf ./nvim/ ~/.config/
-ln -rsf ./config/.bashrc ~/
-ln -rsf ./config/.tmux.conf ~/
-ln -rsf ./config/.gitconfig ~/
+set -e
+
+for file in `find ./config/home/ -type f`; do ln -rsf "$file" ~/; done
 ln -rsf ./config/.ignore ~/workspace/
-ln -rsf ./config/alacritty.yml ~/.config/alacritty/
-ln -rsf ./config/foot.ini ~/.config/foot/
 ln -rsf ./config/firefox/chrome/ ~/.mozilla/firefox/e4xc206r.default-release/
-ln -rsf ./config/config.fish ~/.config/fish/
+ln -rsf ./nvim/ ~/.config/
 ln -rsf ./config/sway/ ~/.config/
 ln -rsf ./config/pipewire/ ~/.config/
+ln -rsf ./config/foot.ini ~/.config/foot/
+ln -rsf ./config/config.fish ~/.config/fish/
+ln -rsf ./config/alacritty.yml ~/.config/alacritty/
