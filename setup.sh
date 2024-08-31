e88a403e0be6f5bd9bc8f48abf0aa1a2ff7afcc7
Tejomay Gadgil <tejomaygadgil@gmail.com>
2024-08-30 20:47:38 -0700

    Add dictionary

--- setup.sh
+++ setup.sh
@@ -7,3 +7,9 @@ brew install fzf ripgrep fd tree z jesseduffield/lazygit/lazygit tmux coreutils 
 # MIT Scheme on M1 Mac
 # https://kennethfriedman.org/thoughts/2021/mit-scheme-on-apple-silicon/
 # https://linux.do/t/topic/33139/4
+
+# Dictionary
+brew install sdcv
+git clone https://github.com/ahacop/websters-dict-1913-stardict.git
+mkdir -p $HOME/.stardict/dic
+tar -xvzf ~/Downloads/websters-dict-1913-stardict/stardict-dictd-web1913-2.4.2.tgz -C $HOME/.stardict/dic
