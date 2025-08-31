193591cfc14b97d0cb2d3d82059da86186475c39
Tejomay Gadgil <tejomay@tejomay.local>
2025-08-30 21:22:10 -0700

    Simplify structure

--- symlink.sh
+++ symlink.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-for file in `find $PWD/cfg/home -type f`; do ln -sf "$file" ~/; done
-ln -sf $PWD/cfg/.ignore ~/workspace/
-ln -sf $PWD/cfg/nvim/ ~/.config/
+for file in `find $PWD/home -type f`; do ln -sf "$file" ~/; done
+ln -sf $PWD/.ignore ~/workspace/
+ln -sf $PWD/nvim/ ~/.config/
