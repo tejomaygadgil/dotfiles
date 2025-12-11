95c92aa4e541ad240df2287e6bae27095c2ce258
Tejomay Gadgil <tejomaygadgil@gmail.com>
2025-12-10 18:37:04 -0800

    Copy hidutil keybinding file

--- symlink.sh
+++ symlink.sh
@@ -7,4 +7,4 @@ ln -sf $PWD/.ignore ~/workspace/
 ln -sf $PWD/nvim/ ~/.config/
 ln -sf $PWD/emacs/init.el ~/.emacs.d/init.el
 ln -sf $PWD/emacs/early-init.el ~/.emacs.d/early-init.el
-ln -sf $PWD/scripts/com.local.KeyRemapping.plist ~/Library/LaunchAgents/com.local.KeyRemapping.plist
+cp -f $PWD/scripts/com.local.KeyRemapping.plist ~/Library/LaunchAgents/
