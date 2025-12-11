e91e3b9e751d7988aae5fd98d2be5e9c78af6d6e
Tejomay Gadgil <tejomay.gadgil@kp.org>
2025-12-10 17:26:47 -0800

    Add git gutter

--- emacs/init.el
+++ emacs/init.el
@@ -54,6 +54,18 @@
 ;; Magit
 (use-package magit
   :ensure t)
+(use-package git-gutter
+  :ensure t)
+;; If you enable global minor mode
+(global-git-gutter-mode t)
+(global-set-key (kbd "C-x C-g") 'git-gutter)
+;; Jump to next/previous hunk
+(global-set-key (kbd "M-p") 'git-gutter:previous-hunk)
+(global-set-key (kbd "M-n") 'git-gutter:next-hunk)
+;; Stage current hunk
+(global-set-key (kbd "C-x v s") 'git-gutter:stage-hunk)
+;; Revert current hunk
+(global-set-key (kbd "C-x v r") 'git-gutter:revert-hunk)
 
 ;; Pdf-tools
 (use-package pdf-tools
